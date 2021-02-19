f88a506b777bb1599f3086bdbcba8ce52aed50ae
Caca404 <christia10k@hotmail.com>
2021-02-19 00:57:19 -0300

    Completo

--- George_Trab/Controllers/CategoriasController.cs
+++ George_Trab/Controllers/CategoriasController.cs
@@ -4,26 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using George_Trab.Models;
+using System.Net;
+using System.Data.Entity;
+using George_Trab.Context;
 
 namespace George_Trab.Controllers
 {
     public class CategoriasController : Controller
     {
-        private static IList<Categoria> categorias = new List<Categoria>()
-        {
-            new Categoria() { CategoriaId = 1, Nome = "Notebooks"},
-            new Categoria() { CategoriaId = 2, Nome = "Monitores"},
-            new Categoria() { CategoriaId = 3, Nome = "Impressoras"},
-            new Categoria() { CategoriaId = 4, Nome = "Mouses"},
-            new Categoria() { CategoriaId = 5, Nome = "Desktops"}
-        };
-
-        // GET: Categorias
+        public Conexao context = new Conexao();
+        // GET: Fabricantes
         public ActionResult Index()
         {
-            return View(categorias);
+            return View(context.Categorias.OrderBy(c => c.Nome));
         }
-        
         public ActionResult Create()
         {
             return View();
@@ -33,40 +27,56 @@ namespace George_Trab.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Categoria c)
         {
-            categorias.Add(c);
-            c.CategoriaId = categorias.Select(m => m.CategoriaId).Max();
+            context.Categorias.Add(c);
+            context.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        public ActionResult Edit(long id)
+        public ActionResult Edit(long? id)
         {
-            return View(categorias.Where(m => m.CategoriaId == id).First());
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Categoria c = context.Categorias.Find(id);
+            if (c == null) return HttpNotFound();
+            return View(c);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(Categoria c)
         {
-            categorias.Remove(categorias.Where(m => m.CategoriaId == c.CategoriaId).First());
-            categorias.Add(c);
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                context.Entry(c).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(c);
         }
 
-        public ActionResult Details(long id)
+        public ActionResult Details(long? id)
         {
-            return View(categorias.Where(m => m.CategoriaId == id).First());
+            if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            Categoria c = context.Categorias.Find(id);
+            if (c == null) return HttpNotFound();
+            return View(c);
         }
 
-        public ActionResult Delete(long id)
+        public ActionResult Delete(long? id)
         {
-            return View(categorias.Where(m => m.CategoriaId == id).First());
+            if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            Categoria c = context.Categorias.Find(id);
+            if (c == null) return HttpNotFound();
+            return View(c);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(Categoria c)
+        public ActionResult Delete(long id)
         {
-            categorias.Remove(categorias.Where(m => m.CategoriaId == c.CategoriaId).First());
+            Categoria c = context.Categorias.Find(id);
+            context.Categorias.Remove(c);
+            context.SaveChanges();
+            TempData["Message"] = "Categoria " + c.Nome.ToUpper() + " foi removido";
             return RedirectToAction("Index");
         }
 
